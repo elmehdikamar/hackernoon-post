05fd86c4b289a861d003bc57ad6717695f96ea9c
El Mehdi Kamar <elmehdikamar@gmail.com>
2021-03-11 00:37:21 +0100

    add footer social links

--- components/footer.js
+++ components/footer.js
@@ -2,8 +2,8 @@ import FooterLink from "./footer_link";
 
 export default function Footer(props) {
     return (
-        <footer className="flex flex-row items-start bg-gray-100 w-full py-2 px-4 mt-10">
-            <ul className="flex-1 flex flex-wrap">
+        <footer className="flex flex-row items-start justify-between bg-gray-100 w-full p-4 mt-10">
+            <ul className="flex flex-wrap max-w-3xl">
                 <FooterLink bold title='Help' />
                 <FooterLink bold title='About' />
                 <FooterLink bold title='Start Writing' />
@@ -21,6 +21,15 @@ export default function Footer(props) {
                 <FooterLink title="Contributors' Club" />
                 <FooterLink title='Chrome Extension' />
             </ul>
+            <div className="flex flex-wrap text-limegreen-600">
+                <a href="#"><i className="mb-3 mr-3 fab fa-linkedin-in fa-lg"></i></a>
+                <a href="#"><i className="mb-3 mr-3 fab fa-facebook-f fa-lg"></i></a>
+                <a href="#"><i className="mb-3 mr-3 fab fa-twitter fa-lg"></i></a>
+                <a href="#"><i className="mb-3 mr-3 fab fa-github fa-lg"></i></a>
+                <a href="#"><i className="mb-3 mr-3 fab fa-youtube fa-lg"></i></a>
+                <a href="#"><i className="mb-3 mr-3 fa fa-podcast fa-lg"></i></a>
+                <a href="#"><i className="mb-3 mr-3 fa fa-rss fa-lg"></i></a>
+            </div>
         </footer>
     )
 }
